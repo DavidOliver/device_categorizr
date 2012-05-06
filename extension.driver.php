9b62d770f09f8a7635a73581942f2d903f7b1ece
David Oliver <david@doliver.co.uk>
2012-05-06 15:05:10 +0200

    Bumping to version 1.0.0. Requiring Symphony 2.3. Tweaking meta info.

--- extension.driver.php
+++ extension.driver.php
@@ -4,19 +4,6 @@ require_once EXTENSIONS . '/device_categorizr/lib/categorizr.php';
 
 class Extension_Device_Categorizr extends Extension {
 	
-	public function about() {
-		return array(
-			'name'			=> 'Device Categorizr',
-			'version'		=> '0.1',
-			'release-date'	=> '2012-03-01',
-			'author'		=> array(
-				'name'			=> 'David Oliver',
-				'website'		=> 'http://doliver.co.uk',
-				'email'			=> 'david@doliver.co.uk'
-			)
-		);
-	}
-	
 	public function getSubscribedDelegates() {
 		return array(
 			array(
